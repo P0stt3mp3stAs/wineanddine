87749f40c861bf31b5b6aefd0191242e33763297
myMac <ghaliwali@gmail.com>
2025-01-09 13:16:34 +0100

    add env and env local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <main>
-      <div className="bg-gray-800 text-white p-4 text-center my-20">
+      <div className="bg-gray-800 text-white p-4 text-center my-30">
         <h1 className="text-3xl font-black">Wine and Dine</h1>
       </div>
       <div className="max-w-4xl mx-auto p-4 text-red-500 text-xl bg-yellow-400 font-black">
